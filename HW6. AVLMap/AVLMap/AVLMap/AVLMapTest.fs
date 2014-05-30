8ff17b267c1fa8930be13d6e2aa05be0fc6441ba
Artem Lobanov <resaglow@gmail.com>
2014-05-30 14:44:30 +0400

    AVLMap update
    Added GetHashCode & IEnumerator test

---
+++ HW6. AVLMap/AVLMap/AVLMap/AVLMapTest.fs
@@ -0,0 +1,14 @@
+﻿module AVLMapTest
+
+open System.Diagnostics
+
+open AVLMap
+
+let mutable myMap = new AVLMap.Map<int, string>([])
+
+for i in [1 .. 10 .. 100] do 
+    myMap <- myMap.Add(i, ("value" + i.ToString()))
+
+for node in myMap do
+    printfn "%A" node
+
