b04c9b6579d7f767264c7e4f9aefe9a99ca86d43
Artem Lobanov <resaglow@gmail.com>
2014-11-20 22:18:58 +0300

    Add Partly Cloudy

---
+++ 3 semester/Cloudy/Cloudy/Program.fs
@@ -0,0 +1,72 @@
+﻿// Learn more about F# at http://fsharp.net
+// See the 'F# Tutorial' project for more help.
+
+module Cloudy
+
+type CreatureType = 
+    | Puppy
+    | Kitten
+    | Hedgehog
+    | Bearcub
+    | Piglet
+    | Bat
+    | Balloon
+
+type DaylightType = 
+    | Morning
+    | Day
+    | Evening
+    | Night
+
+type IDaylight = 
+    abstract member Current : DaylightType
+
+type ILuminary = 
+    abstract member IsShining : bool
+
+type IWind = 
+    abstract member Speed : int
+   
+type Creature(crType:CreatureType) = 
+    member val Type = crType with get
+
+type CourierType = 
+    | Stork
+    | Daemon
+
+type ICourier = 
+    abstract member GiveBaby : Creature -> unit
+
+type IMagic = 
+    abstract member CallDaemon : unit -> ICourier
+    abstract member CallStork : unit -> ICourier
+    
+
+type Cloud(daylight:IDaylight, luminary:ILuminary, wind:IWind, magic:IMagic) = 
+    member private x.InternalCreate() =
+        if luminary.IsShining then
+            if wind.Speed >= 0 && wind.Speed <= 5 then  
+                if daylight.Current = Morning || daylight.Current = Day then (Stork, Puppy) 
+                else (Stork, Kitten)
+            else 
+                if daylight.Current = Morning || daylight.Current = Evening then (Daemon, Hedgehog)
+                else (Stork, Bat)
+        else 
+            if wind.Speed >= 0 && wind.Speed <= 3 then 
+                if daylight.Current = Night then (Daemon, Balloon)
+                else if daylight.Current = Day || daylight.Current = Morning then (Daemon, Bearcub)
+                else (Stork, Piglet)
+            else if wind.Speed >= 4 && wind.Speed <= 7 then (Stork, Balloon)
+
+            else (Daemon, Hedgehog)
+ 
+    member x.Create() =
+        let (courierType, creatureType) = x.InternalCreate()
+        let creature = Creature(creatureType)
+
+        if courierType = Stork then 
+            magic.CallStork().GiveBaby(creature)
+        else 
+            magic.CallDaemon().GiveBaby(creature)
+        
+        creature
---
+++ 3 semester/Cloudy/CloudyTests/Library1.fs
@@ -0,0 +1,59 @@
+﻿module CloudyTests
+
+open Cloudy
+open NUnit.Framework
+open NSubstitute
+open FsCheck
+
+type Generator = 
+    static member int() = Arb.fromGen <| Gen.choose(0, 10)
+
+[<Test>]
+let cloudTest() =         
+    let mockTestProperty(time:DaylightType, isShining:bool, windSpeed:int) =
+        let lightType = Substitute.For<IDaylight>()
+        let luminary = Substitute.For<ILuminary>()
+        let wind = Substitute.For<IWind>() 
+        let magic = Substitute.For<IMagic>()  
+        let courier = Substitute.For<ICourier>()
+
+        lightType.Current.Returns(time) |> ignore 
+        luminary.IsShining.Returns(isShining) |> ignore
+        wind.Speed.Returns(windSpeed) |> ignore 
+        magic.CallStork().Returns(courier) |> ignore
+        magic.CallDaemon().Returns(courier) |> ignore
+
+        let checkOrder (courierType:CourierType) (creatureType:CreatureType) = 
+            if courierType = Stork then 
+                Received.InOrder(fun () ->
+                    magic.CallStork() |> ignore
+                    courier.Received().GiveBaby(Arg.Is<Creature>(fun (creature:Creature) -> creature.Type = creatureType))
+                    )
+            else 
+                Received.InOrder(fun () ->
+                    magic.CallDaemon() |> ignore
+                    courier.Received().GiveBaby(Arg.Is<Creature>(fun (creature:Creature) -> creature.Type = creatureType))
+                    )
+
+        let mockCloud = new Cloud(lightType, luminary, wind, magic)
+
+        mockCloud.Create() |> ignore
+
+        if isShining then
+            if windSpeed >= 0 && windSpeed <= 5 then
+                if time = Morning || time = Day then checkOrder Stork Puppy
+                else checkOrder Stork Kitten
+            else 
+                if time = Morning || time = Evening then checkOrder Daemon Hedgehog
+                else checkOrder Stork Bat
+        else 
+            if windSpeed >= 0 && windSpeed <= 3 then 
+                if time = Night then checkOrder Daemon Balloon
+                else if time = Day || time = Morning then checkOrder Daemon Bearcub
+                else checkOrder Stork Piglet
+            else if windSpeed >= 4 && windSpeed <= 7 then checkOrder Stork Balloon
+
+            else checkOrder Daemon Hedgehog
+    
+    Arb.register<Generator>() |> ignore
+    FsCheck.Check.Verbose(mockTestProperty)
