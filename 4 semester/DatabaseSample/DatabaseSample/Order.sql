3212202e0c9dc479eb96a5daccfbbf53d25f0a50
Artem Lobanov <resaglow@gmail.com>
2015-04-06 10:59:24 +0300

    Database fixes

--- 4 semester/DatabaseSample/DatabaseSample/Order.sql
+++ 4 semester/DatabaseSample/DatabaseSample/Order.sql
@@ -1,10 +1,10 @@
 ﻿CREATE TABLE [dbo].[Order]
 (
     [Number] INT NOT NULL IDENTITY (1,1) PRIMARY KEY, 
-    [Date] DATE NOT NULL, 
-    [Time] TIME NOT NULL,
+    [DateTIme] DATETIME NOT NULL, 
     [Cost] MONEY NOT NULL, 
     [PaymentType] NVARCHAR(MAX) NOT NULL,
     [ClientId] INT NULL FOREIGN KEY REFERENCES [Client]([Id]) ON DELETE SET NULL,    
-    [CarNumber] NCHAR(10) NOT NULL FOREIGN KEY REFERENCES [Car]([Number]) ON UPDATE CASCADE
+    [CarNumber] NCHAR(10) NOT NULL FOREIGN KEY REFERENCES [Car]([Number]) ON UPDATE CASCADE, 
+    [OrderStatus] NVARCHAR(50) NOT NULL
 )
--- 4 semester/DatabaseSample/DatabaseSample/Queries.sql
+++ 4 semester/DatabaseSample/DatabaseSample/Queries.sql
@@ -8,18 +8,18 @@ SELECT * FROM [Car]
 WHERE [Type] <> N'Грузовик' and [Type] <> N'Автобус'
 
 -- 3.	Выбрать номера а/м, в которых число мест >= 5 и упорядочить их по количеству мест.
-SELECT [Number] FROM Car 
-WHERE FreePlacesCount > 5
-ORDER BY FreePlacesCount
+SELECT [Number] FROM [Car ]
+WHERE [FreePlacesCount] > 5
+ORDER BY [FreePlacesCount]
 
 -- 4.	Выбрать номера и фамилии водителей всех белых автомобилей  BMV, у которых в номере встречается цифра 2. Список упорядочить по фамилии водителя.
 SELECT [Number], [Driver] FROM [Car]
-WHERE CHARINDEX('2', [Number]) > 0
-ORDER BY [Driver]
+WHERE CHARINDEX('2', [Number]) > 0 AND [Brand] = N'BMW'
+ORDER BY [Driver] 
 
 -- 5.	Выдать номера автомобилей, время заказа и фамилии водителей, принимавших заказы с 12:00 до 19:00 1 сентября 2005 года.
-SELECT o.[CarNumber], o.[Time], c.[Driver] FROM [Order] o, [Car] c
-WHERE o.[CarNumber] = c.[Number] AND o.[Date] = '2005-09-01' AND o.[Time] BETWEEN '12:00:00' AND '19:00:00'
+SELECT o.[CarNumber], o.[DateTime], c.[Driver] FROM [Order] o, [Car] c
+WHERE o.[CarNumber] = c.[Number] AND o.[DateTime] BETWEEN '2005/09/01 12:00' AND '2005/09/01 19:00'
 
 --Примеры со сложными запросами:
 
@@ -27,8 +27,9 @@ WHERE o.[CarNumber] = c.[Number] AND o.[Date] = '2005-09-01' AND o.[Time] BETWEE
 SELECT SUM([Cost]) FROM [Order]
 
 -- 2.	Получить список водителей, отсортированный по количеству выполненных заказов 
-SELECT c.[Driver] FROM [Car] c, (
+SELECT * FROM [Car] c, (
     SELECT [CarNumber], COUNT([CarNumber]) AS CarOrderCount FROM [Order]
+	WHERE [OrderStatus] = N'Выполнен'
     GROUP BY [CarNumber]
 ) t WHERE c.[Number] = t.[CarNumber]
 ORDER BY CarOrderCount
@@ -36,6 +37,7 @@ ORDER BY CarOrderCount
 -- 3.	Выбрать постоянных клиентов (не менее 5 выполненных заказов)
 SELECT cl.[Name] FROM [Client] cl, (
     SELECT [ClientId], COUNT([ClientId]) AS ClientOrderCount FROM [Order]
+	WHERE [OrderStatus] = N'Выполнен'
     GROUP BY [ClientId]
 ) t WHERE cl.[Id] = t.[ClientId] AND t.ClientOrderCount >= 5
 
